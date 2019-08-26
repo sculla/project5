678b9058bfa9488356a3753bd1cfb30183ed30a1
sculla <sculla@u.washington.edu>
2019-08-26 11:47:16 -0700

    requirements & setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+"""Setup File"""
+
+setup(
+    name='project5',
+    version='.1',
+    packages=[''],
+    url='https://github.com/sculla/project5',
+    license='GNU GENERAL PUBLIC LICENSE',
+    author='sculla',
+    author_email='scull.aa@gmail.com',
+    description='Project 5'
+)

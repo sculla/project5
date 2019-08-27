7d3a994295c96a5e2768498634e21c5d04ec3463
sculla <sculla@u.washington.edu>
2019-08-27 09:06:04 -0700

    morning commit

--- setup.py
+++ setup.py
@@ -1,14 +1,12 @@
 from setuptools import setup
 
-"""Setup File"""
-
 setup(
     name='project5',
     version='.1',
     packages=[''],
     url='https://github.com/sculla/project5',
-    license='GNU GENERAL PUBLIC LICENSE',
+    license='GNU General Public License v3.0',
     author='sculla',
     author_email='scull.aa@gmail.com',
-    description='Project 5'
+    description=''
 )

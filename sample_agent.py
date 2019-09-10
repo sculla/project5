16e6930a6a018a1f571c3ea3b06ae6f9cc2df77f
sculla <sculla@u.washington.edu>
2019-09-10 15:05:59 -0700

    debugging steering step counts

--- sample_agent.py
+++ sample_agent.py
@@ -16,7 +16,7 @@ from keras.utils import plot_model
 import os
 
 random.seed(42)
-INPUT_SHAPE = (64,64)
+INPUT_SHAPE = (64,64,1)
 WINDOW_LENGTH = 5
 run_num = 1
 vision = True
@@ -29,19 +29,15 @@ class TorcsProcessor(Processor):
     def process_observation(self, observation):
 
         focus, speedX, speedY, speedZ, opponents, rpm, track, wheelSpinVel, vision = observation
-
-        vec_input = np.array([[speedX, speedY, speedZ]])
-
-        #image processing
-        assert vision.shape == (4096,3)
+        # image processing
+        assert vision.shape == (4096, 3)
         img = Image.fromarray(vision)
-        img = img.resize(INPUT_SHAPE).convert('L') #grayscale
-        img.tobitmap('thing.bmp')
+        img = img.resize(INPUT_SHAPE).convert('L')  # grayscale
+        #        img.tobitmap('thing.bmp')
         img_input = np.array(img)
-        print(img_input.shape)
         assert img_input.shape == INPUT_SHAPE
-        assert vec_input.shape == (3,)
-        return img_input, vec_input
+        return img_input, speedX, speedY, speedZ
+
 
 class Agent(object):
     def __init__(self, dim_action):
@@ -52,7 +48,10 @@ class Agent(object):
     def init_model(self):
 
         img_input = Input(((WINDOW_LENGTH,) + INPUT_SHAPE))
-        vec_input = Input((WINDOW_LENGTH,3))
+        xvec_input = Input((WINDOW_LENGTH,1))
+        yvec_input = Input((WINDOW_LENGTH,1))
+        zvec_input = Input((WINDOW_LENGTH,1))
+
         x1 = Permute((3,2,1))(img_input)
         x1 = Convolution2D(64, (3, 3), activation='relu')(x1)
         x1 = MaxPool2D()(x1)
@@ -60,14 +59,17 @@ class Agent(object):
         x1 = MaxPool2D()(x1)
         x1 = Convolution2D(16, (3, 3), activation='relu')(x1)
         x1 = MaxPool2D()(x1)
-        # x12 = Concatenate(axis=-1)([x1,vec_input])
+        x2 = Permute((2,1))(xvec_input)
+        x3 = Permute((2,1))(yvec_input)
+        x4 = Permute((2,1))(zvec_input)
         x1 = Flatten()(x1)
-        x2 = Permute((2,1))(vec_input)
         x2 = Flatten()(x2)
-        x12 = Concatenate(axis=-1)([x1,x2])
+        x3 = Flatten()(x3)
+        x4 = Flatten()(x4)
+        x12 = Concatenate(axis=-1)([x1,x2,x3,x4])
         x12 = Dense(256, activation='relu')(x12)
         output = Dense(self.dim_action, activation='tanh')(x12)
-        self.model = Model(inputs=[img_input,vec_input],outputs=[output])
+        self.model = Model(inputs=[img_input,xvec_input,yvec_input,zvec_input],outputs=[output])
         # self.model = Model(inputs=[img_input],outputs=[output])
         plot_model(self.model, to_file='model.png')
         print(self.model.summary())
@@ -82,7 +84,7 @@ env = TorcsEnv(vision=vision, throttle=False)
 
 agent = Agent(dim_action=1)
 
-processor = TorcsProcessor(nb_inputs=2)
+processor = TorcsProcessor(nb_inputs=4)
 
 memory = SequentialMemory(limit=50000, window_length=WINDOW_LENGTH)
 policy = BoltzmannQPolicy()
